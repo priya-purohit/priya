6d357ef51d45512c4473919e6d41da0334219acd
priyanka <priyanka.rajpurohit@cgt.co.in>
2016-01-02 07:29:09 +0530

    Test commit in dev

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
 	echo "priyanka";
 	
-	
-	
+	//git init
+	//$ git add .
+	//$ git add --all
+	//$ git commit -m "initial commit"
+	// git config --global user.email "you@example.com"
+	//git config --global user.name "Your Name"
+
 ?>
